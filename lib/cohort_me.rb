8b2b3474650bcd04b6413309c2338350cef1072d
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-10-08 15:48:25 +0200

    Added activation and activity field

--- lib/cohort_me.rb
+++ lib/cohort_me.rb
@@ -1,19 +1,19 @@
 require "cohort_me/version"
 
 module CohortMe
-
   def self.analyze(options={})
-
     start_from_interval = options[:start_from_interval] || 12
     interval_name = options[:period] || "weeks"
-    activation_class = options[:activation_class] 
+    activation_class = options[:activation_class]
     activation_table_name = ActiveModel::Naming.plural(activation_class)
     activation_user_id = options[:activation_user_id] || "user_id"
-    activation_conditions = options[:activation_conditions] 
+    activation_conditions = options[:activation_conditions]
+    activation_field = options[:activation_field] || "created_at"
 
     activity_class = options[:activity_class] || activation_class
     activity_table_name = ActiveModel::Naming.plural(activity_class)
     activity_user_id = options[:activity_user_id] || "user_id"
+    activity_field = options[:activity_field] || activation_field
 
     period_values = %w[weeks days months]
 
@@ -34,56 +34,47 @@ module CohortMe
       time_conversion = 1.month.seconds
     end
 
-    cohort_query = activation_class.select("#{activation_table_name}.#{activation_user_id}, MIN(#{activation_table_name}.created_at) as cohort_date").group("#{activation_user_id}").where("created_at > ?", start_from)
+    cohort_query = activation_class.select("#{activation_table_name}.#{activation_user_id}, MIN(#{activation_table_name}.#{activation_field}) as cohort_date").group("#{activation_user_id}").where("#{activation_field} > ?", start_from)
 
     if activation_conditions
       cohort_query = cohort_query.where(activation_conditions)
     end
 
     if %(mysql mysql2).include?(ActiveRecord::Base.connection.instance_values["config"][:adapter])
-    
-      select_sql = "#{activity_table_name}.#{activity_user_id}, #{activity_table_name}.created_at, cohort_date, FLOOR(TIMEDIFF(#{activity_table_name}.created_at, cohort_date)/#{time_conversion}) as periods_out"
+
+      select_sql = "#{activity_table_name}.#{activity_user_id}, #{activity_table_name}.#{activity_field}, cohort_date, FLOOR(TIMEDIFF(#{activity_table_name}.#{activity_field}, cohort_date)/#{time_conversion}) as periods_out"
     elsif ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
-      select_sql = "#{activity_table_name}.#{activity_user_id}, #{activity_table_name}.created_at, cohort_date, FLOOR(extract(epoch from (#{activity_table_name}.created_at - cohort_date))/#{time_conversion}) as periods_out"
+      select_sql = "#{activity_table_name}.#{activity_user_id}, #{activity_table_name}.#{activity_field}, cohort_date, FLOOR(extract(epoch from (#{activity_table_name}.#{activity_field} - cohort_date))/#{time_conversion}) as periods_out"
     else
       raise "database not supported"
     end
 
-    data = activity_class.where("created_at > ?", start_from).select(select_sql).joins("JOIN (" + cohort_query.to_sql + ") AS cohorts ON #{activity_table_name}.#{activity_user_id} = cohorts.#{activation_user_id}")
+    data = activity_class.where("#{activity_field} > ?", start_from).select(select_sql).joins("JOIN (" + cohort_query.to_sql + ") AS cohorts ON #{activity_table_name}.#{activity_user_id} = cohorts.#{activation_user_id}")
 
     unique_data = data.all.uniq{|d| [d.send(activity_user_id), d.cohort_date, d.periods_out] }
 
     analysis = unique_data.group_by{|d| convert_to_cohort_date(Time.parse(d.cohort_date.to_s), interval_name)}
-    cohort_hash =  Hash[analysis.sort_by { |cohort, data| cohort }]
+    cohort_hash = Hash[analysis.sort_by { |cohort, data| cohort }]
 
     table = {}
-    cohort_hash.each do |r| 
-
+    cohort_hash.each do |r|
       periods = []
       table[r[0]] = {}
 
-      cohort_hash.size.times{|i| periods << r[1].count{|d| d.periods_out.to_i == i} if r[1]} 
+      cohort_hash.size.times{|i| periods << r[1].count{|d| d.periods_out.to_i == i} if r[1]}
 
       table[r[0]][:count] = periods
       table[r[0]][:data] = r[1]
     end
 
-
-    return table
-
+    table
   end
 
   def self.convert_to_cohort_date(datetime, interval)
-    if interval == "weeks"
-      return datetime.at_beginning_of_week.to_date
-      
-    elsif interval == "days"
-      return Date.parse(datetime.strftime("%Y-%m-%d"))
-
-    elsif interval == "months"
-      return Date.parse(datetime.strftime("%Y-%m-1"))
+    case interval
+    when "weeks";  datetime.at_beginning_of_week.to_date
+    when "days";   Date.parse(datetime.strftime("%Y-%m-%d"))
+    when "months"; Date.parse(datetime.strftime("%Y-%m-01"))
     end
   end
-
-
 end
